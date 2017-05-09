bb1dff7f4ab814f62daad73a8b4e0dfe33dafd3e
harum181 <harum.mualimus.s@gmail.com>
2017-05-10 01:19:06 +0700

    Commit change 222

--- app/src/main/java/com/mcn/apwmu/IdentitasPribadiActivity.java
+++ app/src/main/java/com/mcn/apwmu/IdentitasPribadiActivity.java
@@ -53,6 +53,7 @@ public class IdentitasPribadiActivity extends AppCompatActivity
         }
     }
 //    TODO Update back toolbar
+//    TODO Tambah back button
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
